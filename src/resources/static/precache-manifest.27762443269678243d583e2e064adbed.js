c3d21b73e8cda9a03d54b4477dd93013191c901b
Marcus Weissohn Eede <Munnotubbel@localhost.localdomain>
2020-02-10 23:36:59 +0100

    react build added

---
+++ src/resources/static/precache-manifest.27762443269678243d583e2e064adbed.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecabd643989cb3a83d955c75618e0e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50a66d57a031e5626e30",
+    "url": "/static/css/main.2c88e665.chunk.css"
+  },
+  {
+    "revision": "84d55b0ae300935f409f",
+    "url": "/static/js/2.ff1cd04b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ff1cd04b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a66d57a031e5626e30",
+    "url": "/static/js/main.87b98cae.chunk.js"
+  },
+  {
+    "revision": "e5dab1d1f924c4a35168",
+    "url": "/static/js/runtime-main.e1f58eb2.js"
+  },
+  {
+    "revision": "67d62d876947e52b83cffd69df96a821",
+    "url": "/static/media/TrajanaSans.67d62d87.ttf"
+  },
+  {
+    "revision": "934a4467ea7a1ff79ac3ee2830e8d4f8",
+    "url": "/static/media/TrajanaSans.934a4467.svg"
+  },
+  {
+    "revision": "942a7d287ad5b7daa288482da47ddaa6",
+    "url": "/static/media/TrajanaSans.942a7d28.otf"
+  },
+  {
+    "revision": "a39d98e8132749a33ef1226e300e026c",
+    "url": "/static/media/TrajanaSans.a39d98e8.woff"
+  },
+  {
+    "revision": "bd915118ed50f7077fa34e289ec78b06",
+    "url": "/static/media/TrajanaSans.bd915118.eot"
+  },
+  {
+    "revision": "ee8771f386e4230c2b372994431bd158",
+    "url": "/static/media/TrajanaSans.ee8771f3.woff2"
+  },
+  {
+    "revision": "3f6331cc4f21b919210d4b44e6282082",
+    "url": "/static/media/TrajanaSansBold.3f6331cc.otf"
+  },
+  {
+    "revision": "9dcd8948dfc0fcfa0ed76a2c24d64def",
+    "url": "/static/media/TrajanaSansBold.9dcd8948.ttf"
+  },
+  {
+    "revision": "a9159a1346602652a3c6964afbb3c891",
+    "url": "/static/media/TrajanaSansBold.a9159a13.eot"
+  },
+  {
+    "revision": "cae48b01c373a24eae08035d8942eb23",
+    "url": "/static/media/TrajanaSansBold.cae48b01.svg"
+  },
+  {
+    "revision": "d6bf22f2edf6e88333fa5c85e5950429",
+    "url": "/static/media/TrajanaSansBold.d6bf22f2.woff"
+  },
+  {
+    "revision": "f5fad54fe19967721b85416b94132eb7",
+    "url": "/static/media/TrajanaSansBold.f5fad54f.woff2"
+  },
+  {
+    "revision": "8416ba173d4a9db9bb5f47adbbf4bfd9",
+    "url": "/static/media/TrajanaSansLight.8416ba17.woff2"
+  },
+  {
+    "revision": "9a637241b57379c956d812496049c38d",
+    "url": "/static/media/TrajanaSansLight.9a637241.eot"
+  },
+  {
+    "revision": "9fea4cee9c2137648477321cca099fa1",
+    "url": "/static/media/TrajanaSansLight.9fea4cee.svg"
+  },
+  {
+    "revision": "b49fa735655353f127cf77a6d662ea08",
+    "url": "/static/media/TrajanaSansLight.b49fa735.ttf"
+  },
+  {
+    "revision": "de2d51d596d21c9242fa1ba23f8e7050",
+    "url": "/static/media/TrajanaSansLight.de2d51d5.woff"
+  },
+  {
+    "revision": "f8c6b49e8b784a513f1e49d8ca4ed795",
+    "url": "/static/media/TrajanaSansLight.f8c6b49e.otf"
+  }
+]);
